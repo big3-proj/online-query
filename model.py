a113a8860914fc4ec5ba33ded367d52928489091
Uier <ap9940506@gmail.com>
2021-03-09 14:21:32 +0800

    update

--- model.py
+++ model.py
@@ -6,75 +6,27 @@ from datetime import datetime
 from functools import reduce
 from collections import Counter
 from sklearn.manifold import TSNE
+from os import listdir
 
-filename = 'top_100_users_pushes.csv'
 
-df = pd.read_csv(f'../segmentation/{filename}',
-                 index_col='pusher_id', keep_default_na=False)
-df = df.loc[df['push_tag'] != '']
-art_df = pd.read_csv(
-    f'../segmentation/{filename}', index_col='article_author', keep_default_na=False)
-art_df = art_df.loc[art_df['pusher_id'] == '']
-users = list(set(df.index))
-art_users = list(set(art_df.index))
-
+# load posts
 with open('./posts.json') as f:
     posts = json.load(f)
 
-def parse_timestamp(push_timestamp):
-    [date, time] = push_timestamp.split()
-    time = datetime.strptime(time, '%H:%M:%S').hour
-    return {'date': date, 'time': time}
-
-
-def to_data(data):
-    if len(data) == 2:
-        return {'tag': data[0], **parse_timestamp(data[1])}
-    return {'tag': '', **parse_timestamp(data[0])}
-
-
-def get_activity(user):
-    return list(map(to_data, df.loc[[user], ['push_tag', 'push_timestamp']].values.tolist()))
-
-
-def get_art_activity(user):
-    return list(map(to_data, art_df.loc[[user], ['article_timestamp']].values.tolist()))
-
-
-activities = [get_activity(u) for u in users] + \
-    [get_art_activity(u) for u in art_users]
-
-# activity
-# [
-#     {
-#       'tag': '噓',
-#       'date': '2019-12-28',
-#       'time': 18
-#     },
-#     ...
-# ]
-
-
-def get_online_activity(activity):
-    return list(map(lambda x: json.loads(x), set((map(lambda x: json.dumps({'date': x['date'], 'time': x['time']}), activity)))))
-
 
 # serialization is for unique the list, after encoding it we have to decode it
-online_activities = [get_online_activity(a) for a in activities]
+def get_online_activity(activity):
+    return list(map(lambda x: json.loads(x), set((map(lambda x: json.dumps({'date': x['date'], 'time': x['time']}), activity)))))
 
 
 def get_online_times(activity):
     return Counter(a['time'] for a in activity)
 
 
-online_activities = [[get_online_times(oa).get(
-    i, 0) for i in range(24)] for oa in online_activities]
-
-
-def get_plot():
-    X = np.array(online_activities)
-    X_embedded = TSNE(n_components=2).fit_transform(X)
-    return X_embedded.tolist()
+def get_online_label(activity):
+    label = ['midnight', 'morning', 'afternoon', 'evening']
+    section = [sum(activity[i:i+6]) for i in range(0, 24, 6)]
+    return label[section.index(max(section))]
 
 
 def get_posts():
@@ -85,21 +37,39 @@ def get_post(id):
     return posts[id]
 
 
-def get_plot_of_post(id):
-    post = posts[id]
-    commenters = list(map(lambda x: x['push_userid'], post['messages']))
-    users = list(set([post['author_id']] + commenters))
+def get_plot_of_users(users):
     users_activities = []
+    valid_users = []
     for u in users:
         with open(f'./users/{u}.json') as f:
             user = json.load(f)
         def parse_push(push):
             datetime = push['datetime'].split(' ')
-            datetime[1] = datetime[1].split(':')[0]
-            return { 'tag': '', 'date': datetime[0], 'time': datetime[1] }
-        users_activities.append(list(map(parse_push, user['pushes'])))
+            try:
+                datetime[1] = datetime[1].split(':')[0]
+                return { 'tag': '', 'date': datetime[0], 'time': int(datetime[1]) }
+            except:
+                return None
+        users_activities.append([x for x in list(map(parse_push, user['pushes'])) if x is not None])
+        valid_users.append(u)
     users_online_activities = [get_online_activity(ua) for ua in users_activities]
     users_online_activities = [[get_online_times(oa).get(i, 0) for i in range(24)] for oa in users_online_activities]
+    users_online_label = [get_online_label(u) for u in users_online_activities]
     X = np.array(users_online_activities)
-    X_embedded = TSNE(n_components=2).fit_transform(X)
-    return X_embedded.tolist()
+    X_embedded = TSNE(n_components=2).fit_transform(X).tolist()
+    for i in range(len(X_embedded)):
+        X_embedded[i].append(users_online_label[i])
+        X_embedded[i].append(valid_users[i])
+    return X_embedded
+
+
+def get_plot_of_post(id):
+    post = posts[id]
+    commenters = list(map(lambda x: x['push_userid'], post['messages']))
+    users = list(set([post['author_id']] + commenters))
+    return get_plot_of_users(users)
+    
+
+def get_plot():
+    users = list(map(lambda x: x[:-5], listdir('./users')))
+    return get_plot_of_users(users[:5000])
